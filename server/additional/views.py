81714c52b20363dba6878769d1e2f0176db48dde
Gustavo Moreira <g.hen.moreira@gmail.com>
2025-02-23 00:03:19 -0300

    feat: crud views for additional entity

--- server/additional/views.py
+++ server/additional/views.py
@@ -1,3 +1,43 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializer import AdditionalSerializer
+from .models import Additional
 
-# Create your views here.
+
+class AdditionalListView(generics.ListAPIView):
+    serializer_class = AdditionalSerializer
+    queryset = Additional.objects.all()
+
+
+class AdditionalListByStatus(generics.ListAPIView):
+    serializer_class = AdditionalSerializer
+
+    def get_queryset(self):
+        status = self.request.query_params.get("status", "active")
+        if status == "disabled":
+            return Additional.objects.filter(is_active=False).order_by("name")
+        return Additional.objects.filter(is_active=True).order_by("name")
+
+
+class AdditionalListSoldOutView(generics.ListAPIView):
+    serializer_class = AdditionalSerializer
+
+    def get_queryset(self):
+        return Additional.objects.filter(stock_quantity=0, is_active=False)
+
+
+class AdditionalCreateView(generics.CreateAPIView):
+    serializer_class = AdditionalSerializer
+    queryset = Additional.objects.all()
+
+
+class AdditionalDestroyView(generics.DestroyAPIView):
+    serializer_class = AdditionalSerializer
+    queryset = Additional.objects.all()
+    lookup_field = "id"
+
+
+class AdditionalUpdateView(generics.UpdateAPIView):
+    serializer_class = AdditionalSerializer
+    queryset = Additional.objects.all()
+    lookup_field = "id"
