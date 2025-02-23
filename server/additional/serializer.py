a26e78e5bf24bac7d512ee38b473a71c47f0463b
Gustavo Moreira <g.hen.moreira@gmail.com>
2025-02-23 00:02:46 -0300

    fix: adding return in validation functions in additional serializer

--- server/additional/serializer.py
+++ server/additional/serializer.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
-from models import Additional
+from .models import Additional
 from utils.sales_validations import price_validator, stock_quantity_validator
 
 
-class AdditionalSerializer(serializers.Serializer):
+class AdditionalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Additional
         fields = "__all__"
         read_only_fields = ["id"]
 
-    def validate_quantity(self, value):
-        stock_quantity_validator(value)
+    def validate_stock_quantity(self, value):
+        return stock_quantity_validator(value)
 
     def validate_price(self, value):
-        price_validator(value)
+        return price_validator(value)
