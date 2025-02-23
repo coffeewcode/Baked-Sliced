29a33c7ec0e2e864d92ca418bf17184ede5519e0
Gustavo Moreira <g.hen.moreira@gmail.com>
2025-02-23 00:08:39 -0300

    feat: category crud views

--- server/product/views.py
+++ server/product/views.py
@@ -1,17 +1,18 @@
 from django.forms import ValidationError
 from django.shortcuts import render
-from rest_framework import generics
-from .serializer import ProductSerializer
-from .models import Product
+from rest_framework import generics, status
+from .serializer import ProductSerializer, ProductListSerializer, CategorySerializer
+from .models import Product, Category
+from rest_framework.response import Response
 
 
 class ProductListView(generics.ListAPIView):
-    serializer_class = ProductSerializer
+    serializer_class = ProductListSerializer
     queryset = Product.objects.all()
 
 
 class ProductListByStatusView(generics.ListAPIView):
-    serializer_class = ProductSerializer
+    serializer_class = ProductListSerializer
 
     def get_queryset(self):
         status = self.request.query_params.get("status", "active")
@@ -27,19 +28,25 @@ class ProductCreateView(generics.CreateAPIView):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer=serializer)
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
 
 class ProductDestroyView(generics.DestroyAPIView):
-    querysets = Product.objects.all()
+    queryset = Product.objects.all()
     serializer_class = ProductSerializer
     lookup_field = "id"
 
-    def perform_destroy(self, instance: Product) -> None:
+    def perform_destroy(self, instance):
         if instance.is_active:
             raise ValidationError(
-                f"The product {instance.name} is active. Disable its to delete."
+                f"The product {instance.name} is active. Disable it to delete."
             )
         return super().perform_destroy(instance)
+
+    def handle_exception(self, exc):
+        if isinstance(exc, ValidationError):
+            return Response({"detail": str(exc)}, status=status.HTTP_400_BAD_REQUEST)
+        return super().handle_exception(exc)
 
 
 class ProductPatchView(generics.UpdateAPIView):
@@ -49,16 +56,37 @@ class ProductPatchView(generics.UpdateAPIView):
 
     def perform_update(self, serializer):
         instance = serializer.save()
-        if instance.is_active and instance.quantity <= 0:
+        if instance.is_active and instance.stock_quantity <= 0:
             raise ValidationError("Active products must have a positive stock")
-        if instance.quantity == 0:
+        if instance.stock_quantity == 0:
             instance.is_active = False
             instance.save()
         return instance
 
 
 class ProductListSoldOut(generics.ListAPIView):
-    serializer_class = ProductSerializer
+    serializer_class = ProductListSerializer
 
     def get_queryset(self):
-        return Product.objects.filter(quantity=0, is_active=False)
+        return Product.objects.filter(stock_quantity=0, is_active=False)
+
+
+class CategoryListView(generics.ListAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+
+
+class CategoryCreateView(generics.CreateAPIView):
+    serializer_class = CategorySerializer
+
+
+class CategoryPatchView(generics.UpdateAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+    lookup_field = "id"
+
+
+class CategoryDestroyView(generics.DestroyAPIView):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all()
+    lookup_field = "id"
