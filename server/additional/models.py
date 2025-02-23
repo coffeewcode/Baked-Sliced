afa2203af847968333de8d311e39b290d1c9e095
Gustavo Moreira <g.hen.moreira@gmail.com>
2025-02-23 00:01:05 -0300

    feat: adding is_active property and description property as optional

--- server/additional/models.py
+++ server/additional/models.py
@@ -6,9 +6,10 @@ from utils.decrement_functions import decrement_stock_default
 class Additional(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=70)
-    description = models.CharField(max_length=300)
+    description = models.CharField(max_length=300, blank=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     stock_quantity = models.IntegerField(default=0)
+    is_active = models.BooleanField(default=True)
 
     def __str__(self):
         return self.name
