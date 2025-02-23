b43b6f51c9ec98d357b8b39f26c8cf12b355c516
Gustavo Moreira <g.hen.moreira@gmail.com>
2025-02-22 23:56:21 -0300

    rfeat: adding additional_available prop as optional

--- server/product/models.py
+++ server/product/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from additional.models import Additional
 import uuid
 
-from utils.decrement_functions import decrement_stock_default
+from utils.decrement_functions import decrement_stock_default, increment_stock_default
 
 
 class Category(models.Model):
@@ -18,12 +18,14 @@ class Product(models.Model):
     id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
     name = models.CharField(max_length=70)
     description = models.CharField(max_length=300)
-    observation = models.CharField(max_length=200)
     category = models.ForeignKey(
-        Category, on_delete=models.CASCADE, related_name="products"
+        Category,
+        on_delete=models.CASCADE,
     )
     stock_quantity = models.IntegerField(default=0)
-    additional_available = models.ManyToManyField(Additional, related_name="products")
+    additional_available = models.ManyToManyField(
+        Additional, related_name="products", blank=True
+    )
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
@@ -31,3 +33,6 @@ class Product(models.Model):
 
     def decrement_stock(self, quantity):
         decrement_stock_default(self, quantity, self.name)
+
+    def increment_stock(self, quantity):
+        increment_stock_default(self, quantity)
