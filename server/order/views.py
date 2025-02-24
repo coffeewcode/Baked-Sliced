66205a5ecce3c35e3d9bfd9603aec9c394fcf433
Gustavo Moreira <g.hen.moreira@gmail.com>
2025-02-23 21:37:26 -0300

    feat: main crud views for order app

--- server/order/views.py
+++ server/order/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.forms import ValidationError
+from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .serializer import (
+    OrderItemListSerializer,
+    OrderItemSerializer,
+    OrderItemAdditionalSerializer,
+)
+from .models import OrderItem, OrderItemAdditional
 
-# Create your views here.
+
+class OrderItemListView(generics.ListAPIView):
+    serializer_class = OrderItemListSerializer
+    queryset = OrderItem.objects.all()
+
+
+class OrderItemCreateView(generics.CreateAPIView):
+    serializer_class = OrderItemSerializer
+    queryset = OrderItem.objects.all()
+
+
+class OrderItemDestroyView(generics.DestroyAPIView):
+    serializer_class = OrderItemSerializer
+    queryset = OrderItem.objects.all()
+    lookup_field = "id"
+
+
+class OrderItemPatchView(generics.UpdateAPIView):
+    serializer_class = OrderItemSerializer
+    queryset = OrderItem.objects.all()
+    lookup_field = "id"
+
+
+class OrderItemAdditionalCreateView(generics.CreateAPIView):
+    serializer_class = OrderItemAdditionalSerializer
+    queryset = OrderItemAdditional.objects.all()
+
+    def create(self, request):
+        serializer = self.get_serializer(request=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer=serializer)
+        return Response(data=serializer.data, status=status.HTTP_201_CREATED)
