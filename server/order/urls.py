6bb9f96bfc9bb1b367d2b0536ee47b1bb59ffa06
Gustavo Moreira <g.hen.moreira@gmail.com>
2025-03-01 21:54:21 -0300

    feat: urls view for order item additional manipulation

--- server/order/urls.py
+++ server/order/urls.py
@@ -11,5 +11,11 @@ urlpatterns = [
     order_include(views.OrderItemCreateView.as_view(), "create"),
     order_include(views.OrderItemDestroyView.as_view(), "delete/<uuid:id>"),
     order_include(views.OrderItemPatchView.as_view(), "update/<uuid:id>"),
-    order_include(views.OrderItemAdditionalCreateView.as_view(), "additional/create"),
+    order_include(
+        views.OrderItemAdditionalDestroyView.as_view(), "additional/delete/<uuid:id>"
+    ),
+    order_include(
+        views.OrderItemAdditionalUpdateView.as_view(), "additional/update/<uuid:id>"
+    ),
+    order_include(views.OrderItemAdditionalListView.as_view(), "additional/list"),
 ]
