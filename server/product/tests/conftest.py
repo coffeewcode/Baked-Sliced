320397be23cc8ec1fef7227c713422e222b33a7b
Gustavo Moreira <g.hen.moreira@gmail.com>
2025-03-04 14:20:40 -0300

    docs: commenting in all the fixture objects to make it easier to understand

--- server/product/tests/conftest.py
+++ server/product/tests/conftest.py
@@ -25,6 +25,8 @@ def product_with_valid_data(category):
 
 @pytest.fixture
 def additional_with_valid_data(db):
+    """Creates and returns a Additional objects."""
+
     def _create_additional(is_active=True, stock_quantity=5):
         additional_1 = Additional.objects.create(
             name="Extra Cheese",
@@ -45,6 +47,7 @@ def additional_with_valid_data(db):
 
 @pytest.fixture
 def active_additional():
+    """Creates and returns a Additional with property is_active equal True to validate it."""
     return Additional.objects.create(
         name="Active Additional", is_active=True, price=5, stock_quantity=10
     )
@@ -52,6 +55,7 @@ def active_additional():
 
 @pytest.fixture
 def inactive_additional():
+    """Creates and returns a Additional with property is_active equal False to validate it."""
     return Additional.objects.create(
         name="Inactive Additional", is_active=False, price=5, stock_quantity=10
     )
@@ -59,6 +63,7 @@ def inactive_additional():
 
 @pytest.fixture
 def out_of_stock_additional():
+    """Creates and returns a Additional out of stock to validate it."""
     return Additional.objects.create(
         name="Out of Stock Additional", is_active=True, price=5, stock_quantity=0
     )
@@ -66,6 +71,7 @@ def out_of_stock_additional():
 
 @pytest.fixture
 def product(category):
+    """Creates and returns a Product to validate the incremental and decrement stock."""
     return Product.objects.create(
         name="Test Product",
         description="Test Description",
