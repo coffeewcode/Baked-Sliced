4374b0afd67ae62e64ce46b21222ad1ea3444ec8
matsvincenzi <matsvincenzi@gmail.com>
2025-03-05 20:32:27 -0300

    feat: adding new urls

--- server/_main/urls.py
+++ server/_main/urls.py
@@ -4,17 +4,14 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 
-
 @api_view(["GET"])
 def test_api(request):
     return Response(
         {"success": "The api is connected and working"}, status=status.HTTP_200_OK
     )
 
-
 def api_include(module):
     return path("api/", include(module))
-
 
 urlpatterns = [
     path("admin/", admin.site.urls),
@@ -22,4 +19,6 @@ urlpatterns = [
     api_include("product.urls"),
     api_include("order.urls"),
     api_include("additional.urls"),
+    api_include("address.urls"), 
+    api_include("delivery.urls"),
 ]
